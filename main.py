9996892d15b550ae61cc10c42e2e4793679014d6
unknown <Zekun>
2017-10-24 01:50:52 -0500

    main

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 """main function running the whole puzzle solver."""
 
-from utility import *
+from utility import read_board, valid_neighbors
 
 filename = 'puzzles/input55.txt'
 
 
 def flow_free_main():
+    """flow_free_main.
 
+    DESCRIPTION: main function of the solver.
+    INPUT: None
+    OUTPUT: None
+    """
     with open(filename, 'r', encoding='UTF-8') as input:
         board, colors = read_board(input)
 
     print(len(board))
-    print(colors)
-    print(valid_neighbors(4,4,5,5))
+    print(len(colors))
+    print(valid_neighbors(4, 4, 5, 5))
+
 
 if __name__ == '__main__':
 
