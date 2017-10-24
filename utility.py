a13ec45a31563870644700ca28deaa67de3e6a37
MrCaiting <cwu72@illinois.edu>
2017-10-23 23:20:13 -0500

    Utility File Modified

--- utility.py
+++ utility.py
@@ -1,18 +1,34 @@
 """utility.py file."""
 import itertools as itt
-# Assign Binary values for each directions
+
+# Some useful majic numbers
+PAIR = 2
+
+# Assign binary values for each directions
 UP = 0b0001         # decimal 1
 DOWN = 0b0010       # decimal 2
 LEFT = 0b0100       # decimal 4
 RIGHT = 0b1000      # decimal 8
 
+# Assign binary values for the direction for each flow
+UPDOWN = UP | DOWN
+LRIGHT = LEFT | RIGHT
+UPRIGHT = UP | RIGHT
+UPLEFT = UP | LEFT
+DRIGHT = DOWN | LEFT
+DLEFT = DOWN | RIGHT
+
 # Define all the possible movement on board with corresponding coordinate changes
 MOVES = [(UP, 1, 0), (DOWN, -1, 0), (LEFT, 0, -1), (RIGHT, 0, 1)]
 
+# Define all the possible flow direction structures
+FLOWDIR = [UPDOWN, LRIGHT,
+           UPRIGHT, UPLEFT,
+           DRIGHT, DLEFT]
+
 
 def read_board(file):
-    """
-    read_board.
+    """read_board.
 
     DESCRIPTION: utility function used to read the .txt board display file and
         to return the board in the format of an array and a dictionary of all
@@ -36,32 +52,48 @@ def read_board(file):
                     colors[cell] = color_index
     return board, colors
 
-def in_bound(i,j,width,height):
-    #check if a cell in on board
-    if i >= 0 and i < width and j >=0 and j < height:
+
+def in_bound(i, j, width, height):
+    """in_bound.
+
+    DESCRIPTION: check if a cell in on board
+    """
+    if i >= 0 and i < width and j >= 0 and j < height:
         return True
 
-def get_neighbors(i,j):
-    #get all neighbors of a cell
+
+def get_neighbors(i, j):
+    """get_neighbors.
+
+    DESCRIPTION: get all neighbors of a cell
+    """
     neighbors = []
     for (direction, x, y) in MOVES:
         neighbors.append((direction, i+x, j+y))
     return neighbors
 
-def valid_neighbors(i,j,width,height):
-    #check for neighbors that is in bound
+
+def valid_neighbors(i, j, width, height):
+    """valid_neighbors.
+
+    DESCRIPTION: check for neighbors that is in bound
+    """
     neighbors = []
-    for (direction, x, y) in get_neighbors(i,j):
+    for (direction, x, y) in get_neighbors(i, j):
         if in_bound(x, y, width, height):
             neighbors.append((direction, x, y))
     return neighbors
 
+
 def x_or(variables):
-    #define function that performs logical XOR,
-    # ie. returns list that any two of the
-    # variables are different.
+    """x_or.
+
+    DESCRIPTION: define function that performs logical XOR,
+        ie. returns list that any two of the
+        variables are different.
+    """
     xor_list = []
-    combos = itt.combinations(variables,2)
+    combos = itt.combinations(variables, PAIR)
     for (x, y) in combos:
         xor_list.append((-x, -y))
     return xor_list
